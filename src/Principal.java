cf2b3f00e02ff9de1eb81644f99b11f99a0698a6
Nelson Torres <nelson.torres.lagos@gmail.com>
2025-11-15 00:20:34 -0300

    Se corrigen opciones en el menú del usuario

--- src/Principal.java
+++ src/Principal.java
@@ -9,24 +9,25 @@ public class Principal {
         ArrayList<String> historial = new ArrayList<>();
 
         int opcion = 0;
-        while (opcion != 7) {
-            System.out.println("\n--- Conversor de Monedas ---");
-            System.out.println("===============================================");
-            System.out.println("[1]. USD a CLP");
-            System.out.println("[2]. USD a ARS");
-            System.out.println("[3]. ARS a USD");
-            System.out.println("[4]. BRL a USD");
-            System.out.println("[5]. USD a EUR");
-            System.out.println("[6]. EUR a USD");
-            System.out.println("[7]. Salir");
-            System.out.println("[8]. Historial de conversiones");
-            System.out.println("===============================================\n");
-            System.out.print("Ingrese una opción: \n");
+        while (opcion != 8) {
+            System.out.println("\n+-----------------------------------------------+");
+            System.out.println("|    Bienvenido a Mi Conversor de Monedas       |");
+            System.out.println("+-----------------------------------------------+");
+            System.out.println("[1]. USD -> CLP");
+            System.out.println("[2]. USD -> ARS");
+            System.out.println("[3]. ARS -> USD");
+            System.out.println("[4]. BRL -> USD");
+            System.out.println("[5]. USD -> EUR");
+            System.out.println("[6]. EUR -> USD");
+            System.out.println("[7]. HISTORIAL DE CONVERSIONES");
+            System.out.println("[8]. SALIR");
+            System.out.println("**********************************************\n");
+            System.out.print("[Seleccione una opción (1-8):] \n");
 
             opcion = scanner.nextInt();
 
             if (opcion >= 1 && opcion <= 6) {
-                System.out.print("Ingrese el monto a convertir: ");
+                System.out.print("Ingrese el monto de dinero a convertir: ");
                 double monto = scanner.nextDouble();
 
                 String monedaBase = "";
@@ -42,32 +43,34 @@ public class Principal {
                 }
 
                 Convertidor resultado = api.obtenerTasa(monedaBase, monedaDestino);
-                System.out.println("Tasa de Conversion" + resultado.getConversion_rate());
+                System.out.println("Tasa de Conversion Utilizada: " + resultado.getConversion_rate());
 
                 if (resultado != null) {
-                    // Calculamos el valor final
+                    // Calcular la conversión final y enviar al Convertidor
                     resultado.setResult(String.valueOf(monto * resultado.getConversion_rate()));
                     System.out.println(resultado.mostrarResultado(monto) + "\n");
-
-
+                    // Registrar la Transaccion para revisión
                     String registro = resultado.mostrarResultado(monto);
                     historial.add(registro);
                 }
-            } else if (opcion == 8) {
-                System.out.println("+----------------------------------------------+");
-                System.out.println("+        [Revisión de Historial de Uso]        + \n");
-                System.out.println("+----------------------------------------------+");
-                System.out.println("::conversiones:: \n");
-                for (String r: historial) {
-                    System.out.println(r);
-                    System.out.println("<<<<<<<<<<< Fin del Registro >>>>>>>>>>>>>>>");
+            } else if (opcion == 7) {
+                if (historial.isEmpty()) {
+                    System.out.println("*********[No hay historial para mostrar]**********");
+                } else {
+                    System.out.println("+----------------------------------------------+");
+                    System.out.println("+        [Revisión de Historial de Uso]        +");
+                    System.out.println("+----------------------------------------------+");
+                    System.out.println("\n");
+                    for (String r : historial) {
+                        System.out.println(r);
+                        System.out.println("*******************************************");
+                    }
                 }
-            } else if (opcion != 7) {
-                    System.out.println("Opción inválida. Intente nuevamente.");
-                }
+            } else if (opcion != 8) {
+                System.out.println("@@@@@@@ Opción No Valida, Intente nuevamente @@@@@@@");
+            }
         }
-
-        System.out.println("!Programa Finalizado¡ mm(00)mm ¡Regrese Pronto!");
+        System.out.println("!Programa Finalizado con Exito¡ mm(00)mm ¡Vuelva Pronto!");
     }
 }
 
